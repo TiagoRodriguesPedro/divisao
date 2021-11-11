087a9a9e20c2a31e33e2e941346c0751c8bbc0b8
Tiago Rodrigues <isatiago098@gmail.com>
2021-11-11 13:16:03 -0300

    resolucao exercicio divisao

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerador, denominador;
+
+            // digite o numerador
+            Console.Write("digite um numerador ---");
+            numerador = Convert.ToInt32(Console.ReadLine());
+
+            // digite o denominador
+            Console.WriteLine("denominador---");
+           numerador = Convert.ToInt32(Console.ReadLine());
+
+            // 12 dividido por 3 é 12
+            if (numerador > 12 / 3 );
+            {
+             Console.WriteLine(" demoninador");
+            }
+            
+        }
+    }
+}
